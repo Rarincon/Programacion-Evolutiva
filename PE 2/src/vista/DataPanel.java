72179ac5e42402f3c57ee8f4816b4b08077a809d
Rarincon <60478235+Rarincon@users.noreply.github.com>
2021-04-09 21:44:37 +0200

    Vistas de Soluciones

--- PE 2/src/vista/DataPanel.java
+++ PE 2/src/vista/DataPanel.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -15,6 +18,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -34,11 +38,13 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
@@ -50,50 +56,93 @@ public class DataPanel extends JPanel implements AlgoritmoGenObserver{
 	
 	private static final long serialVersionUID = 1L;
 	private Border _defaultBorder = BorderFactory.createLineBorder(Color.black, 2);	
-	private static final String[] paneles = {"Media", "Objetivo", "Objetivo Genotipos", "Mejor Actual", "Mejor Actual Genotipos"};
-	private List<JLabel> labels;
-
+	//private static final String[] paneles = {"Media", "Objetivo", "Objetivo Genotipos", "Mejor Actual", "Mejor Actual Genotipos"};
+	private JLabel fit,abec, descif;
+	static private final String abecedario="a b c d e f g h i j k l m n o p q r s t u v w x y z";
+	static private final int TAM= 26;
+	
 	private Controller _ctrl;
+	private int fitness;
+	private String descifrado;
 	
 	public DataPanel(Controller c) {
 		_ctrl=c;
+		fitness=0;
+		descifrado= "";
+		//Arrays.fill(descifrado,"");
 		createData();
 		_ctrl.addObserver(this);
 	}
 	
 	
-	private void createData() {
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		setBorder(BorderFactory.createTitledBorder(_defaultBorder, "Soluciones", TitledBorder.LEFT, TitledBorder.TOP));
-		JPanel p = new JPanel();
-		labels = new ArrayList<JLabel>();
-		for (int i = 0; i < paneles.length;i++) {
-			JLabel a = new JLabel(paneles[i]);
-			a.setPreferredSize(new Dimension(1000, 15));
-			labels.add(a);
-			p.add(a);
-		}
+	private void createData() {		
+		//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new BorderLayout());
+		//setLayout(new FlowLayout(FlowLayout.CENTER));
+		setBorder(BorderFactory.createTitledBorder(_defaultBorder, "Solucion", TitledBorder.LEFT, TitledBorder.TOP));
 		
-		p.setVisible(true);
-		p.setPreferredSize(new Dimension(500, 200));
-		this.add(p);
-		this.setPreferredSize(new Dimension(200, 130));
-		setVisible(true);
+		JPanel p = new JPanel();
+		//p.setLayout(new GridLayout(4,1,20,75));
+		p.setLayout(new GridLayout(5,1));
+		p.setPreferredSize(new Dimension(50,50));
+		
+		//p.setBorder(BorderFactory.createTitledBorder(_defaultBorder, "", TitledBorder.LEFT, TitledBorder.TOP));
+		JLabel x= new JLabel("Cromosoma");
+		x.setFont(new Font("Comic Sans MS", Font.PLAIN, 17));
+		x.setVerticalAlignment(SwingConstants.CENTER);
+		JPanel x0 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		x0.add(x);
+		String aux= "Fitness: "+ String.valueOf(fitness);
+		fit = new JLabel(aux);
+		JPanel x1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		x1.add(fit);
+		abec = new JLabel(abecedario);
+		JPanel x2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		x2.add(abec);
+		descif= new JLabel(descifrado);
+		JPanel x3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		x3.add(descif);
+		
+		fit.setVerticalAlignment(SwingConstants.CENTER);
+		abec.setVerticalAlignment(SwingConstants.CENTER);
+		descif.setVerticalAlignment(SwingConstants.CENTER);
+		
+		JSeparator sep= new JSeparator(SwingConstants.HORIZONTAL);
+		sep.setPreferredSize(new Dimension(10,50));
+		sep.setForeground(Color.black);
+		
+		//p.add(x);
+		p.add(x1);
+		p.add(x2);
+		p.add(sep);
+		p.add(x3);
+		
+		//Chapucilla
+		add(x0, BorderLayout.NORTH);
+		add(new JLabel("                 "), BorderLayout.WEST);
+		add(p, BorderLayout.CENTER);
+		add(new JLabel("                 "), BorderLayout.EAST);
+		add(new JLabel(" "), BorderLayout.SOUTH);
+		setPreferredSize(new Dimension(200, 130));
 	}
 
 
 	@Override
 	public void update(int generation, Map<String, Object> stats) {
 		
-		for (int i = 0; i < paneles.length;i++)
-			labels.get(i).setText(paneles[i] + ":   " + stats.get(paneles[i]).toString());
+		fitness=(int) stats.get("fitness");
+		descifrado=(String) stats.get("descifrado");
+		String aux= "Fitness: "+ String.valueOf(fitness);
+		fit.setText(aux);
+		descif.setText(descifrado);
+		
 	}
-
 
 	@Override
 	public void reset() {
-		for (int i = 0; i < paneles.length;i++)
-			labels.get(i).setText(paneles[i]);
+		
+		fit.setText("Fitness: --");
+		descif.setText("");
 	}
 	
 }
