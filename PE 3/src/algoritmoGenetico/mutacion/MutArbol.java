1158faf0da63bd0de82be77874aa37008764cad5
roberl234 <robeasun@ucm.es>
2021-05-09 19:28:51 +0200

    mutArbol casi terminado

--- PE 3/src/algoritmoGenetico/mutacion/MutArbol.java
+++ PE 3/src/algoritmoGenetico/mutacion/MutArbol.java
@@ -1,20 +1,44 @@
 package algoritmoGenetico.mutacion;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import algoritmoGenetico.individuos.Arbol;
 import algoritmoGenetico.individuos.Individuo;
 
 public class MutArbol extends Mutacion{
 
 	public MutArbol(double _probMutacion) {
 		super(_probMutacion);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public List<Individuo> mutarInd(List<Individuo> p) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Individuo> mutarInd(List<Individuo> pob) { //Mirar si lo hace bn
+		List<Individuo> nuevaPob= new ArrayList<Individuo>();
+		double prob;
+		for(int i=0; i<pob.size(); i++) {
+			nuevaPob.add(pob.get(i).copia());
+			
+			prob=Math.random();
+			if(prob < probMutacion) {
+				Random rnd = new Random();
+				Arbol a = nuevaPob.get(i).getArbol().copia();
+				List<String> l=a.toArray();
+				int s = (int) (Math.random()*l.size());			
+				int prof= (int) (Math.random()*3);
+				Arbol b=new Arbol(prof);
+				b.inicializacionCompleta(0,1);
+				
+				//Aqui hay que borrar ese trozo del arbol antes de insertar b
+				
+				a.insert(b,s);
+				
+				nuevaPob.get(i).setArbol(a);
+			}
+		}
+		return nuevaPob;
+		
 	}
 
 }
