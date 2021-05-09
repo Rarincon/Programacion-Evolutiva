3d1fbc924ad034465382300bad2137cb54911c7e
roberl234 <robeasun@ucm.es>
2021-05-09 18:57:09 +0200

    MutacionFuncionalSimple

--- PE 3/src/algoritmoGenetico/mutacion/FuncionSimple.java
+++ PE 3/src/algoritmoGenetico/mutacion/FuncionSimple.java
@@ -1,20 +1,44 @@
 package algoritmoGenetico.mutacion;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import algoritmoGenetico.individuos.Arbol;
 import algoritmoGenetico.individuos.Individuo;
 
-public class FuncionSimple extends Mutacion {
+public class FuncionSimple extends Mutacion{
 
 	public FuncionSimple(double _probMutacion) {
 		super(_probMutacion);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public List<Individuo> mutarInd(List<Individuo> p) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Individuo> mutarInd(List<Individuo> pob) { //Mirar si lo hace bn
+		List<Individuo> nuevaPob= new ArrayList<Individuo>();
+		double prob;
+		for(int i=0; i<pob.size(); i++) {
+			nuevaPob.add(pob.get(i).copia());
+			
+			prob=Math.random();
+			if(prob < probMutacion) {
+				Random rnd = new Random();
+				ArrayList<Arbol> nodos= new ArrayList<Arbol>();
+				Arbol a = nuevaPob.get(i).getArbol().copia();
+				a.getFunciones(a.getHijos(), nodos);
+				int func = rnd.nextInt(Individuo.funciones.length);
+				
+				int s = (int) (Math.random()*nodos.size());			
+				
+				Arbol b = nodos.get(s).copia();
+				b.setValor(Individuo.funciones[func]);
+				a.insertFuncion(a.getHijos(), b, s, 0);
+				
+				nuevaPob.get(i).setArbol(a);
+			}
+		}
+		return nuevaPob;
+		
 	}
 
 }
