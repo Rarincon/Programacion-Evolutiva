c90b5af232295ee81c0f6eb4513dae3da1518aea
Rarincon <60478235+Rarincon@users.noreply.github.com>
2021-05-15 20:02:21 +0200

    Expansion hecha

--- PE 3/src/algoritmoGenetico/mutacion/Expansion.java
+++ PE 3/src/algoritmoGenetico/mutacion/Expansion.java
@@ -1,20 +1,45 @@
 package algoritmoGenetico.mutacion;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import algoritmoGenetico.individuos.Arbol;
 import algoritmoGenetico.individuos.Individuo;
 
 public class Expansion extends Mutacion{
 
 	public Expansion(double _probMutacion) {
 		super(_probMutacion);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public List<Individuo> mutarInd(List<Individuo> p) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Individuo> mutarInd(List<Individuo> pob) {
+		List<Individuo> nuevaPob= new ArrayList<Individuo>();
+		double prob;
+		for(int i=0; i<pob.size(); i++) {
+			nuevaPob.add(pob.get(i).copia());
+			
+			prob=Math.random();
+			if(prob < probMutacion) {
+				ArrayList<Arbol> nodos= new ArrayList<Arbol>();
+				Arbol a = nuevaPob.get(i).getArbol().copia();
+				a.getTerminales(a.getHijos(), nodos);
+				
+				int s = (int) (Math.random()*nodos.size());	
+				int prof=a.getMax_prof();
+				if(prof>1)prof--;
+				Arbol b= new Arbol(prof);
+				b.inicializacionCompleta(0, 0);				
+				
+				a.insertTerminal(a.getHijos(), b, s, 0);
+				
+				a.profundidad(b.getHijos(),1,0);
+				int nod = a.obtieneNodos(a, 0);
+				a.setNumNodos(nod);
+				nuevaPob.get(i).setArbol(a);			
+			}
+		}
+		return nuevaPob;
 	}
 
 }
